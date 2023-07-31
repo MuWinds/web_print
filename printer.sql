ee46f2c214774220bdf5ffc08e15b2c49a0457c7
MuWinds <93832089+MuWinds@users.noreply.github.com>
2023-08-01 00:48:32 +0800

    add sql file

---
+++ printer.sql
@@ -0,0 +1,37 @@
+﻿# Host: localhost  (Version: 5.7.26)
+# Date: 2023-08-01 00:46:52
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'password');
+
+#
+# Structure for table "user_info"
+#
+
+CREATE TABLE `user_info` (
+  `account` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`account`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user_info"
+#
+
+INSERT INTO `user_info` VALUES (1,'account');
