fd38b9edec2bd548a4e230a7ea2813604b912860
MuWinds <93832089+MuWinds@users.noreply.github.com>
2023-08-01 00:50:47 +0800

    Update index.py
    Kill UserRegister class

--- views/index.py
+++ views/index.py
@@ -67,26 +67,6 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         self.write('hello，正在跳转')
         self.redirect('/user-uploadfile')
-class UserRegisterHanlder(tornado.web.RequestHandler):
-    def post(self, *args, **kwargs):
-        context={}
-        email=self.get_argument('email',default=None)
-        password=self.get_argument('password')
-        password_again=self.get_argument('password_again')
-        if len(password)<8:
-            context['password_length_error']='密码至少8个字符'
-        if password_again!=password:
-            context['password_input error']='两次密码输入不一致'
-        else:
-            db = application.Application().db
-            password=base64.b64decode(password.encode('utf-8'))
-            sql='insert into user (password) values (%s)'
-            db.insert(sql,password)
-            account=random.randint(10000000,99999999)
-            sql=' insert into user_info (account,email) values (%s,%s)'
-            db.insert(sql,account,email)
-            context['status']='200'
-        self.write(context)
 class UserLoginHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         return self.render('login.html')
